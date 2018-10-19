af50912817da1982466f14bcd7d20c8228325b2c
rafaelmarra <rafael.mrnt@gmail.com>
2018-10-18 21:05:09 -0300

    classe DiceSelector e ViewInits

---
+++ app/src/main/java/com/example/rafael/dah/DiceSelector.kt
@@ -0,0 +1,13 @@
+package com.example.rafael.dah
+
+class DiceSelector() {
+
+    var d20 = true
+    var d4 = false
+    var d6 = false
+    var d8 = false
+    var d10 = false
+    var d12 = false
+    var dPercent = false
+
+}
--- app/src/main/java/com/example/rafael/dah/MainActivity.kt
+++ app/src/main/java/com/example/rafael/dah/MainActivity.kt
@@ -5,17 +5,32 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 
 import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
+
+    val diceSelector = DiceSelector()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViews()
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    fun initViews(){
+        val btnD20 = findViewById<Button>(R.id.btn_d20)
+        val btnD4 = findViewById<Button>(R.id.btn_d4)
+        val btnD8 = findViewById<Button>(R.id.btn_d8)
+        val btnD10 = findViewById<Button>(R.id.btn_d10)
+        val btnD12 = findViewById<Button>(R.id.btn_d12)
+        val btnDPercent = findViewById<Button>(R.id.btn_dPercent)
     }
 }
